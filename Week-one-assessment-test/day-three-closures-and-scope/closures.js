aa12125bdad8d8dc344ef243c88f22054ff81bec
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2026-03-22 01:09:10 +0530

    code added

--- Week-one-assessment-test/day-three-closures-and-scope/closures.js
+++ Week-one-assessment-test/day-three-closures-and-scope/closures.js
@@ -1,15 +1,26 @@
 // Build a Counter Application using closures
 
-function createcounter() {
-  let count = 0;
-  return function () {
+function createcounter() { 
+  let count = 0; //ye privat hai count ha isko access nahi kar sakat hai
+  return function () {   
     count++;
     return count;
   };
 }
 
-const counter = createcounter();
+const counter = createcounter(); // ye private nahi hai
 
 console.log(counter()); //1
 console.log(counter()); //2
 console.log(counter()); //3
+
+
+// count initial value 0 hai abhi
+// count = 0    
+// count ++ 1
+// count = 1
+// count 1+1
+// count 2
+// count 2+1
+// count 3
+// 1 2 3 output
