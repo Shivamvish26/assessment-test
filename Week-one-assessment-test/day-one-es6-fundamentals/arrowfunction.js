8d6154982f936598b642bfc02ea0c90a071b961b
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2026-03-21 23:37:32 +0530

    code added

--- Week-one-assessment-test/day-one-es6-fundamentals/arrowfunction.js
+++ Week-one-assessment-test/day-one-es6-fundamentals/arrowfunction.js
@@ -1,8 +1,17 @@
 // Create functions using arrow functions
 
+// with arrow function
 const data = (a,b) =>{
+    return a + b
+}
+
+// normal function
+function userdata  (a,b){
     return a + b
 }
 
 let result = data(4,5)
 console.log(result)
+
+// let result = userdata(4,5)
+// console.log(result)
